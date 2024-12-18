79270793bbdaf0c24f3077bf37efdab20baeee5c
Isaac Cambron <icambron@gmail.com>
2024-12-18 02:28:10 -0500

    faster day 17

--- src/day17.rs
+++ src/day17.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 use std::ops::{Shl, Shr, ShrAssign};
 use itertools::Itertools;
 use crate::advent::{Solver};
@@ -20,37 +22,34 @@ impl Solver for Day17 {
     }
 
     fn part_1(&self, computer: &mut Self::Input) -> String {
-        let output = run(computer, false);
+        let mut output: Vec<u64> = vec![];
+        while let Some(o) = computer.run_until_output() {
+            output.push(o);
+        }
         output.iter().join(",")
     }
 
     fn part_2(&self, computer: &mut Self::Input) -> String {
+        let max_index = computer.instructions_raw.len() - 1;
+        let mut solutions: BinaryHeap<Sol> = BinaryHeap::from([Sol { a: 0, iteration: 0 }]);
 
-        let len = computer.instructions_raw.len();
+        while let Some(sol) = solutions.pop() {
+            let goal = computer.instructions_raw[max_index - sol.iteration];
+            let val = sol.a.shl(3);
+            for a in val..val + 8 {
+                computer.reset(a);
+                let output = computer.run_until_output();
 
-        let mut solutions: Vec<u64> = vec![0];
-
-        for i in (0..len).rev() {
-            let goal = computer.instructions_raw[i];
-            let mut next_solutions = vec![];
-
-            for sol in solutions.iter() {
-                let sol = sol.shl(3);
-                for a in sol..sol + 8 {
-                    computer.reg_a = a;
-                    let output = run(computer, true);
-
-                    if *output.first().unwrap() == goal {
-                        next_solutions.push(a);
+                if output == Some(goal) {
+                    if sol.iteration == max_index {
+                        return a.to_string();
                     }
+
+                    solutions.push(Sol { a, iteration: sol.iteration + 1 });
                 }
             }
-
-            solutions = next_solutions;
         }
-
-        solutions.sort();
-        solutions[0].to_string()
+        "No solution found".to_string()
     }
 
     fn expected(&self) -> (&'static str, &'static str) {
@@ -62,39 +61,22 @@ impl Solver for Day17 {
     }
 }
 
-fn run(computer: &mut Computer, halt_at_first_output: bool) -> Vec<u64> {
+#[derive(Debug, PartialEq, Eq)]
+struct Sol {
+    a: u64,
+    iteration: usize
+}
 
-    let mut instruction_pointer: u8 = 0;
-    let mut output: Vec<u64> = vec![];
-
-    while let Some(i) = computer.instructions.get(instruction_pointer as usize) {
-        match i {
-            Instruction::Bxl(arg) => computer.reg_b ^= *arg as u64,
-
-            Instruction::Jnz(arg) => {
-                if computer.reg_a != 0 {
-                    instruction_pointer = *arg / 2;
-                    continue
-                }
-            }
-
-            Instruction::Bst(combo) => computer.reg_b = computer.arg(combo) & 0x7,
-            Instruction::Bxc => computer.reg_b ^= computer.reg_c,
-            Instruction::Out(combo) => {
-                output.push(computer.arg(combo) & 0x7);
-                if halt_at_first_output {
-                    return output;
-                }
-            },
-            Instruction::Adv(combo) => computer.reg_a.shr_assign(computer.arg(combo)),
-            Instruction::Bdv(combo) => computer.reg_b = computer.reg_a.shr(computer.arg(combo)),
-            Instruction::Cdv(combo) => computer.reg_c = computer.reg_a.shr(computer.arg(combo)),
-        }
-
-        instruction_pointer += 1;
+impl PartialOrd<Self> for Sol {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
+}
 
-    output
+impl Ord for Sol {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.a.cmp(&self.a)
+    }
 }
 
 fn parse_instructions(txt: &str) -> (Vec<Instruction>, Vec<u64>) {
@@ -128,6 +110,7 @@ fn parse_instructions(txt: &str) -> (Vec<Instruction>, Vec<u64>) {
 
 #[derive(Debug, Clone)]
 pub struct Computer {
+    instruction_pointer: usize,
     reg_a: u64,
     reg_b: u64,
     reg_c: u64,
@@ -145,6 +128,7 @@ impl Computer {
             reg_c,
             instructions,
             instructions_raw,
+            instruction_pointer: 0,
         }
     }
 
@@ -155,6 +139,40 @@ impl Computer {
             Combo::RegB => self.reg_b,
             Combo::RegC => self.reg_c,
         }
+    }
+    
+    fn reset(&mut self, reg_a: u64) {
+        self.instruction_pointer = 0;
+        self.reg_a = reg_a;
+    }
+
+    fn run_until_output(&mut self) -> Option<u64> {
+        while let Some(i) = self.instructions.get(self.instruction_pointer) {
+            match i {
+                Instruction::Bxl(arg) => self.reg_b ^= *arg as u64,
+
+                Instruction::Jnz(arg) => {
+                    if self.reg_a != 0 {
+                        self.instruction_pointer = (*arg / 2) as usize;
+                        continue
+                    }
+                }
+
+                Instruction::Bst(combo) => self.reg_b = self.arg(combo) & 0x7,
+                Instruction::Bxc => self.reg_b ^= self.reg_c,
+                Instruction::Out(combo) => {
+                    self.instruction_pointer += 1;
+                    return Some(self.arg(combo) & 0x7)
+                },
+                Instruction::Adv(combo) => self.reg_a.shr_assign(self.arg(combo)),
+                Instruction::Bdv(combo) => self.reg_b = self.reg_a.shr(self.arg(combo)),
+                Instruction::Cdv(combo) => self.reg_c = self.reg_a.shr(self.arg(combo)),
+            }
+
+            self.instruction_pointer += 1;
+        }
+
+        None
     }
 }
 
@@ -187,46 +205,5 @@ impl Combo {
             6 => Self::RegC,
             _ => panic!("Unknown combo")
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_1() {
-        let mut computer = Computer::parse(0, 0, 9, "2,6");
-        run(&mut computer, false);
-        assert_eq!(computer.reg_b, 1);
-    }
-
-    #[test]
-    fn test_2() {
-        let mut computer = Computer::parse(10, 0, 0, "5,0,5,1,5,4");
-        let output = run(&mut computer, false);
-        assert_eq!(output, vec![0, 1, 2]);
-    }
-
-    #[test]
-    fn test_3() {
-        let mut computer = Computer::parse(2024, 0, 0, "0,1,5,4,3,0");
-        let output = run(&mut computer, false);
-        assert_eq!(output, vec![4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]);
-        assert_eq!(computer.reg_a, 0);
-    }
-
-    #[test]
-    fn test_4() {
-        let mut computer = Computer::parse(0, 29, 0, "1,7");
-        run(&mut computer, false);
-        assert_eq!(computer.reg_b, 26);
-    }
-
-    #[test]
-    fn test_5() {
-        let mut computer = Computer::parse(0, 2024, 43690, "4,0");
-        run(&mut computer, false);
-        assert_eq!(computer.reg_b, 44354);
     }
 }
