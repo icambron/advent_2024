b52a862cb60b24d80d2d6e26a831491adef11bc6
Isaac Cambron <icambron@gmail.com>
2025-10-24 14:08:07 -0400

    day 24 part 2 but kinda gross

--- src/day24.rs
+++ src/day24.rs
@@ -93,8 +93,81 @@ impl Solver for Day24 {
         final_number.to_string()
     }
 
-    fn part_2(&self, _input: &mut Self::Input) -> String {
-        todo!()
+    fn part_2(&self, input: &mut Self::Input) -> String {
+        let mut connections: HashMap<u32, Vec<&Gate>> = HashMap::new();
+        for gate in &input.gates {
+            connections.entry(gate.left_wire).or_default().push(gate);
+            connections.entry(gate.right_wire).or_default().push(gate);
+        }
+
+        // println!("Connections: {:?}", connections);
+
+        // assume blithly that there's no issue in the first bit, but we still need the carry wire
+        let and = find_gate(&connections, encode("x00"), GateType::And).unwrap();
+        let mut prev_carry_wire = and.to;
+
+        let mut bad_wires = Vec::new();
+
+        for i in 1..45 {
+            let mut bad_this_iteration = None;
+            println!("i: {}", format!("x{:02}", i));
+            let x_wire = encode(&format!("x{:02}", i));
+
+            // we're going to assume xn and yn are never swapped with anything
+            let xor1 = find_gate(&connections, x_wire, GateType::Xor).unwrap();
+            let and1 = find_gate(&connections, x_wire, GateType::And).unwrap();
+
+            // also assume the incoming carry isn't swapped, because if it is, we'll find it the round before and provide the swapped one
+            println!("Carry wire: {}", decode(prev_carry_wire));
+            let xor2 = find_gate(&connections, prev_carry_wire, GateType::Xor).unwrap();
+            let and2 = find_gate(&connections, prev_carry_wire, GateType::And).unwrap();
+
+            // whatever xor1's output is, it had better be hooked up to xor2
+            if xor2.left_wire == prev_carry_wire && xor2.right_wire != xor1.to
+                || xor2.right_wire == prev_carry_wire && xor2.left_wire != xor1.to
+            {
+                bad_wires.push(xor1.to);
+                bad_this_iteration = Some(xor1.to);
+            }
+
+            // xor2 produces the output value, so if it's not an output, it's wrong
+            if is_z(xor2.to).is_none() {
+                bad_wires.push(xor2.to);
+                bad_this_iteration = Some(xor2.to);
+            }
+
+            // the last one doesn't have a carry
+            if i == 44 {
+                continue;
+            }
+
+            let or = match find_gate(&connections, and2.to, GateType::Or) {
+                None => {
+                    bad_wires.push(and2.to);
+                    bad_this_iteration = Some(and2.to);
+
+                    // if we couldn't find the or from and2, then we need to find it from and1's output; they can't both be wrong
+                    find_gate(&connections, and1.to, GateType::Or).unwrap()
+                }
+                Some(or) => {
+                    // make sure and1 is the other side of it
+                    if or.left_wire == and2.to && or.right_wire != and1.to || or.right_wire == and2.to && or.left_wire != and1.to {
+                        bad_wires.push(and1.to);
+                        bad_this_iteration = Some(and1.to);
+                    }
+                    or
+                }
+            };
+
+            if is_z(or.to).is_some() {
+                bad_wires.push(or.to);
+                prev_carry_wire = bad_this_iteration.unwrap();
+            } else {
+                prev_carry_wire = or.to;
+            }
+        }
+
+        bad_wires.into_iter().map(decode).sorted().join(",")
     }
 
     fn expected(&self) -> (&'static str, &'static str) {
@@ -106,12 +179,28 @@ impl Solver for Day24 {
     }
 }
 
+fn find_gate<'a>(connections: &'a HashMap<u32, Vec<&'a Gate>>, one_side: u32, t: GateType) -> Option<&'a Gate> {
+    connections
+        .get(&one_side)
+        .into_iter()
+        .flatten()
+        .find(|&gate| (gate.left_wire == one_side || gate.right_wire == one_side) && gate.op == t)
+        .copied()
+}
+
 fn encode(input: &str) -> u32 {
     let bytes = input.as_bytes();
     let a = bytes[0] as u32;
     let b = bytes[1] as u32;
     let c = bytes[2] as u32;
     a << 16 | b << 8 | c
+}
+
+fn decode(input: u32) -> String {
+    let a = ((input >> 16) & 0xff) as u8 as char;
+    let b = ((input >> 8) & 0xff) as u8 as char;
+    let c = (input & 0xff) as u8 as char;
+    format!("{}{}{}", a, b, c)
 }
 
 fn is_z(input: u32) -> Option<u32> {
@@ -146,7 +235,7 @@ enum GateSide {
     Right,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum GateType {
     Or,
     Xor,
